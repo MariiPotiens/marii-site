697973f24ec0701cd64227ca21c1c82d0bb42f45
MariiPotiens <145818820+MariiPotiens@users.noreply.github.com>
2026-01-30 12:36:15 -0300

    Create page.js

---
+++ app/page.js
@@ -0,0 +1,16 @@
+export default function Home() {
+  return (
+    <main className="container">
+      <h1>Marii</h1>
+      <p>Streamer & Criadora de Conteúdo</p>
+
+      <div className="links">
+        <a href="https://www.youtube.com/@mariiii" target="_blank">▶️ YouTube</a>
+        <a href="https://www.twitch.tv/mariiii_i" target="_blank">🎮 Twitch</a>
+        <a href="https://x.com/mariiiii_i" target="_blank">🐦 Twitter / X</a>
+        <a href="https://www.instagram.com/marii_potiens/" target="_blank">📸 Instagram</a>
+        <a href="https://www.tiktok.com/@mariiii_i" target="_blank">🎵 TikTok</a>
+      </div>
+    </main>
+  );
+}
